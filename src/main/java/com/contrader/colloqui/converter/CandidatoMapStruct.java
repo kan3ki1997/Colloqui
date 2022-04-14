3f179fb0c7425ad1981fe788e5f878fc1ff6ccf1
Contrader <niki.pera@hotmail.it>
2022-04-14 23:29:49 +0200

    Gateway init

--- src/main/java/com/contrader/colloqui/converter/CandidatoMapStruct.java
+++
@@ -1,9 +0,0 @@
-package com.contrader.colloqui.converter;
-
-import com.contrader.colloqui.dto.CandidatoDTO;
-import com.contrader.colloqui.model.Candidato;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CandidatoMapStruct extends MapStruct<Candidato, CandidatoDTO> {
-}
