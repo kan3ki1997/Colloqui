b1fe6cef49d7bc2d9a785c2cff886da565a3d494
Contrader <niki.pera@hotmail.it>
2022-04-15 15:40:03 +0200

    annotation e pom fixati

--- src/main/java/com/contrader/colloqui/ColloquiApplication.java
+++ src/main/java/com/contrader/colloqui/ColloquiApplication.java
@@ -6,11 +6,13 @@ import com.contrader.colloqui.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ColloquiApplication {
 
     public static void main(String[] args) {
