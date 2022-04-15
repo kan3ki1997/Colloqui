d454531f3777c2621e6426b8725450ba52fbe830
Contrader <niki.pera@hotmail.it>
2022-04-15 17:15:29 +0200

    CorsConfiguration aggiunto

--- src/main/java/com/contrader/colloqui/CorsConfiguration.java
+++ src/main/java/com/contrader/colloqui/CorsConfiguration.java
@@ -1,4 +1,27 @@
 package com.contrader.colloqui;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration { // classe obbligatoria per i browser
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*")); // serve per far accedere solo a determinate risorse
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PUT","DELETE"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 }
