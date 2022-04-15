a7e375118cd72003e1957ed1420cf4bedfd45211
Contrader <niki.pera@hotmail.it>
2022-04-15 10:43:42 +0200

    FIX server

--- src/main/java/com/contrader/colloqui/ColloquiApplication.java
+++ src/main/java/com/contrader/colloqui/ColloquiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class ColloquiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ColloquiApplication.class, args);
